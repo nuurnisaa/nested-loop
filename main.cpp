e491fbef82bd827c07ca27990e2314b2532230d6
Nuur <103619888+nuurnisaa@users.noreply.github.com>
2023-02-19 15:29:54 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numGrid[3][2]= {
+
+
+
+    {1,2},
+    {4,5},
+    {9,6}
+
+
+    };
+
+    for (int i=0; i<3; i++)
+    {
+        for(int j=0; j<2; j++){
+            cout<<numGrid[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
